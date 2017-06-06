6dee5193a309acbe77b0e5453a5fa4cdcef484b9
Diego Rueda <diego.rueda2@gmail.com>
2017-06-05 21:18:17 -0400

    validacion-de-datos-de-un-formulario-en-laravel-5-3-con-tdd

--- tests/features/CreatePostsTest.php
+++ tests/features/CreatePostsTest.php
@@ -46,5 +46,13 @@ class CreatePostsTest extends FeatureTestCase
         //then
     }
 
-
+    public function test_create_post_form_validation()
+    {
+        $this->actingAs($this->defaultUser())
+            ->visit(route('posts.create'))
+            ->press('Publicar')
+            ->seePageIs(route('posts.create'))
+            ->seeInElement('#field_title.has-error .help-block', 'El campo título es obligatorio')
+            ->seeInElement('#field_content.has-error .help-block', 'El campo contenido es obligatorio');
+    }
 }
