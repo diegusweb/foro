6ff57fc02bd5a4074f9c3e263b4d4c9ec243d230
Diego Rueda <diego.rueda2@gmail.com>
2017-06-07 22:47:01 -0400

    Verificar features ya desarrollados usando pruebas de regresión en Laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,8 +10,14 @@ class PostController extends Controller
     //
 
 
-    public function show(Post $post)
+    public function show(Post $post, $slug)
     {
+       // abort_unless($post->slug != $slug, 404);
+
+        if($post->slug != $slug){
+            return redirect($post->url, 301);
+        }
+
         return view('posts.show', compact('post'));
     }
 }
--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -20,12 +20,48 @@ class ShowPostTest extends TestCase
 
         $user->posts()->save($post);
 
-        dd(route('posts.show', $post));
+       // dd(route('posts.show', $post));
+        //dd($post->url);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
     }
+
+    function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+        $user->posts()->save($post);
+        $url = $post->url;
+        $post->update(['title' => 'New title']);
+        $this->visit($url)
+            ->seePageIs($post->url);
+    }
+
+    /*public function test_post_url_with_wrong_slugs_still()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->get($url)
+            ->assertResponseStatus(404);
+
+    }*/
+
 }
