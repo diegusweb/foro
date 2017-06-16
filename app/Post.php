dbbc84e6dd0ba4e25fbe8e19d5029d8b8bf1e2f2
Diego Rueda <diego.rueda2@gmail.com>
2017-06-15 22:16:51 -0400

    Soporte de Markdown en laravel

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -38,5 +39,10 @@ class Post extends Model
     public function getUrlAttribute()
     {
         return route('posts.show', [$this->id, $this->slug]);
+    }
+
+    public function getSafeHtmlContentAttribute()
+    {
+        return Markdown::convertToHtml(e($this->content));
     }
 }
